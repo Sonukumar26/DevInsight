e7cccfcc3d52fec17d053d1631966ab533083b92
sonu kumar <sonukumarroy1000@gmail.com>
2026-07-03 01:52:39 +0530

    codereview and history page

---
+++ client/src/pages/CodeReviewPage.jsx
@@ -0,0 +1,12 @@
+import CodeReview from "../components/CodeReview";
+
+function CodeReviewPage() {
+  return (
+    <>
+      <h2>AI Code Review</h2>
+      <CodeReview />
+    </>
+  );
+}
+
+export default CodeReviewPage;
---
+++ client/src/pages/HistoryPage.jsx
@@ -0,0 +1,12 @@
+import AnalysisHistory from "../components/AnalysisHistory";
+
+function HistoryPage() {
+  return (
+    <>
+      <h2>Analysis History</h2>
+      <AnalysisHistory />
+    </>
+  );
+}
+
+export default HistoryPage;
